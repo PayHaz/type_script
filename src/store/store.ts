59ce9f373614fc152a1489a8fad974d5d3bad044
Vadim Ermakov <vadim.ermakov-89@mail.ru>
2022-12-11 23:28:24 +0500

    Added sessions with cookies

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import authorizationSlice from './authorization'
 import { configureStore } from '@reduxjs/toolkit'
+import tokenSlice from './token'
 import contentSlice from './content'
 
 export const store = configureStore({
 	reducer: {
 		content: contentSlice,
 		authorization: authorizationSlice,
+		token: tokenSlice,
 	},
 })
 
